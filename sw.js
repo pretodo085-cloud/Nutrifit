b270c56c09117d1b1839689be0cce137bc425577
pretodo085-cloud <preto.do.085@gmail.com>
2026-09-19 16:38:56 -0300

    Add files via upload
    Atualiza NutriFit para versão final PWA

--- sw.js
+++ sw.js
@@ -1,41 +1,17 @@
-const CACHE_NAME = "nutrifit-v1";
-
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest"
-];
-
+const CACHE_NAME = "nutrifit-v3";
+const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-192.svg","./icon-512.svg"];
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-
-  const url = new URL(event.request.url);
-
-  if (url.origin === location.origin) {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
+  if(event.request.method !== "GET") return;
+  const url=new URL(event.request.url);
+  if(url.origin===location.origin){
+    event.respondWith(caches.match(event.request).then(r=>r||fetch(event.request)));
   }
 });
